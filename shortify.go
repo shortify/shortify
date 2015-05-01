ccb0f78766dd3a0aca99882cb9d72b31fe9ff0d8
pseudomuto <david.muto@gmail.com>
2015-05-01 16:39:38 -0400

    making the executable load the config file

--- shortify.go
+++ shortify.go
@@ -5,13 +5,21 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
 const serverPort = ":8080"
 
 func main() {
-	if !shortify.HandleCommandLine(os.Args) {
-		router := shortify.NewRouter()
-		log.Fatal(http.ListenAndServe(serverPort, router))
+	if shortify.Configure(configFilePath()) {
+		if !shortify.HandleCommandLine(os.Args) {
+			router := shortify.NewRouter()
+			log.Fatal(http.ListenAndServe(serverPort, router))
+		}
 	}
+}
+
+func configFilePath() string {
+	file, _ := filepath.Abs(os.Args[0])
+	return file + ".gcfg"
 }
